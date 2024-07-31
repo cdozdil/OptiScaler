7b760b3383cd095af1e61a8f86a5679c99926a9a
cdozdil <cdozdil@gmail.com>
2024-08-01 00:31:18 +0300

    add rcas to base dx11 feature

--- OptiScaler/backends/IFeature_Dx11.h
+++ OptiScaler/backends/IFeature_Dx11.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "IFeature.h"
 #include "../imgui/Imgui_Dx11.h"
+#include "../rcas/RCAS_Dx11.h"
 
 class IFeature_Dx11 : public virtual IFeature
 {
@@ -8,6 +9,8 @@ protected:
 	ID3D11Device* Device = nullptr;
 	ID3D11DeviceContext* DeviceContext = nullptr;
 	inline static std::unique_ptr<Imgui_Dx11> Imgui = nullptr;
+	std::unique_ptr<RCAS_Dx11> RCAS = nullptr;
+
 
 public:
 	virtual bool Init(ID3D11Device* InDevice, ID3D11DeviceContext* InContext, NVSDK_NGX_Parameter* InParameters) = 0;
