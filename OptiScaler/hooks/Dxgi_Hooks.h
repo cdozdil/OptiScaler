f6717c1a04c4126380808e9f4152fe8d6d0fbf09
cdozdil <cdozdil@gmail.com>
2024-08-15 17:22:59 +0300

    stash

--- OptiScaler/hooks/Dxgi_Hooks.h
+++ OptiScaler/hooks/Dxgi_Hooks.h
@@ -4,12 +4,16 @@
 
 #include <dxgi1_6.h>
 
+typedef void(*CleanCallback)(SwapchainSource, bool);
+typedef void(*PresentCallback)(SwapchainSource, IDXGISwapChain*);
+typedef void(*ReleaseCallback)(SwapchainSource, HWND);
+
 namespace Hooks
 {
     void AttachDxgiHooks();
     void DetachDxgiHooks();
 
-    void SetDxgiClean(std::function<void(SwapchainSource, bool)>);
-    void SetDxgiPresent(std::function<void(SwapchainSource, IDXGISwapChain)>);
-    void SetDxgiRelease(std::function<void(SwapchainSource, HWND)>);
+    void SetDxgiClean(CleanCallback);
+    void SetDxgiPresent(PresentCallback);
+    void SetDxgiRelease(ReleaseCallback);
 }
