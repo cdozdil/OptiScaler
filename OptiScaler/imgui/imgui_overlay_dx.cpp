b92e52c6a1ea11f5ee019eaa496fc1a88c6522df
cdozdil <cdozdil@gmail.com>
2024-09-25 10:51:45 +0300

    fix for hkD3D11CreateDeviceAndSwapChain

--- OptiScaler/imgui/imgui_overlay_dx.cpp
+++ OptiScaler/imgui/imgui_overlay_dx.cpp
@@ -1075,14 +1075,9 @@ static HRESULT hkD3D11CreateDeviceAndSwapChain(IDXGIAdapter* pAdapter, D3D_DRIVE
                 }
 
                 LOG_DEBUG("created new swapchain: {0:X}, hWnd: {1:X}", (UINT64)buffer, (UINT64)pSwapChainDesc->OutputWindow);
+                *ppSwapChain = new WrappedIDXGISwapChain4(real == nullptr ? buffer : real, d3d11Device, pSwapChainDesc->OutputWindow, Present, CleanupRenderTarget);
+                LOG_DEBUG("created new WrappedIDXGISwapChain4: {0:X}, pDevice: {1:X}", (UINT64)buffer, (UINT64)d3d11Device);
             }
-            else
-            {
-                LOG_DEBUG("created new swapchain: {0:X}", (UINT64)buffer);
-            }
-
-            *ppSwapChain = new WrappedIDXGISwapChain4(real == nullptr ? buffer : real, d3d11Device, pSwapChainDesc->OutputWindow, Present, CleanupRenderTarget);
-            LOG_DEBUG("created new WrappedIDXGISwapChain4: {0:X}, pDevice: {1:X}", (UINT64)buffer, (UINT64)d3d11Device);
         }
 
         if (buf != nullptr)
