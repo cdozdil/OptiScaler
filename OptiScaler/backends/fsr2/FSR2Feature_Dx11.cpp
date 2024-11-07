ee726800257506ec749b773e2f865ed3486e4942
cdozdil <cdozdil@gmail.com>
2024-11-07 23:49:45 +0300

    use d3d11 constants

--- OptiScaler/backends/fsr2/FSR2Feature_Dx11.cpp
+++ OptiScaler/backends/fsr2/FSR2Feature_Dx11.cpp
@@ -288,31 +288,31 @@ bool FSR2FeatureDx11::Evaluate(ID3D11DeviceContext* InContext, NVSDK_NGX_Paramet
     if (!RCAS->IsInit())
         Config::Instance()->RcasEnabled = false;
 
-    ID3D11ShaderResourceView* restoreSRVs[128] = {};
-    ID3D11SamplerState* restoreSamplerStates[16] = {};
-    ID3D11Buffer* restoreCBVs[15] = {};
-    ID3D11UnorderedAccessView* restoreUAVs[8] = {};
+    ID3D11ShaderResourceView* restoreSRVs[D3D11_COMMONSHADER_INPUT_RESOURCE_SLOT_COUNT] = {};
+    ID3D11SamplerState* restoreSamplerStates[D3D11_COMMONSHADER_SAMPLER_SLOT_COUNT] = {};
+    ID3D11Buffer* restoreCBVs[D3D11_COMMONSHADER_CONSTANT_BUFFER_API_SLOT_COUNT] = {};
+    ID3D11UnorderedAccessView* restoreUAVs[D3D11_1_UAV_SLOT_COUNT] = {};
 
     // backup compute shader resources
-    for (size_t i = 0; i < 128; i++)
+    for (size_t i = 0; i < D3D11_COMMONSHADER_INPUT_RESOURCE_SLOT_COUNT; i++)
     {
         restoreSRVs[i] = nullptr;
         InContext->CSGetShaderResources(i, 1, &restoreSRVs[i]);
     }
 
-    for (size_t i = 0; i < 16; i++)
+    for (size_t i = 0; i < D3D11_COMMONSHADER_SAMPLER_SLOT_COUNT; i++)
     {
         restoreSamplerStates[i] = nullptr;
         InContext->CSGetSamplers(i, 1, &restoreSamplerStates[i]);
     }
 
-    for (size_t i = 0; i < 15; i++)
+    for (size_t i = 0; i < D3D11_COMMONSHADER_CONSTANT_BUFFER_API_SLOT_COUNT; i++)
     {
         restoreCBVs[i] = nullptr;
         InContext->CSGetConstantBuffers(i, 1, &restoreCBVs[i]);
     }
 
-    for (size_t i = 0; i < 8; i++)
+    for (size_t i = 0; i < D3D11_1_UAV_SLOT_COUNT; i++)
     {
         restoreUAVs[i] = nullptr;
         InContext->CSGetUnorderedAccessViews(i, 1, &restoreUAVs[i]);
@@ -641,28 +641,28 @@ bool FSR2FeatureDx11::Evaluate(ID3D11DeviceContext* InContext, NVSDK_NGX_Paramet
     }
 
     // restore compute shader resources
-    for (size_t i = 0; i < 128; i++)
+    for (size_t i = 0; i < D3D11_COMMONSHADER_INPUT_RESOURCE_SLOT_COUNT; i++)
     {
         if (restoreSRVs[i] != nullptr)
-            InContext->CSGetShaderResources(i, 1, &restoreSRVs[i]);
+            InContext->CSSetShaderResources(i, 1, &restoreSRVs[i]);
     }
 
-    for (size_t i = 0; i < 16; i++)
+    for (size_t i = 0; i < D3D11_COMMONSHADER_SAMPLER_SLOT_COUNT; i++)
     {
         if (restoreSamplerStates[i] != nullptr)
-            InContext->CSGetSamplers(i, 1, &restoreSamplerStates[i]);
+            InContext->CSSetSamplers(i, 1, &restoreSamplerStates[i]);
     }
 
-    for (size_t i = 0; i < 15; i++)
+    for (size_t i = 0; i < D3D11_COMMONSHADER_CONSTANT_BUFFER_API_SLOT_COUNT; i++)
     {
         if (restoreCBVs[i] != nullptr)
-            InContext->CSGetConstantBuffers(i, 1, &restoreCBVs[i]);
+            InContext->CSSetConstantBuffers(i, 1, &restoreCBVs[i]);
     }
 
-    for (size_t i = 0; i < 8; i++)
+    for (size_t i = 0; i < D3D11_1_UAV_SLOT_COUNT; i++)
     {
         if (restoreUAVs[i] != nullptr)
-            InContext->CSGetUnorderedAccessViews(i, 1, &restoreUAVs[i]);
+            InContext->CSSetUnorderedAccessViews(i, 1, &restoreUAVs[i], 0);
     }
 
     _frameCount++;
