6f5c2158777387b16c6d02b021f6d7d94296bfb8
cdozdila <cdozdil@gmail.com>
2025-04-30 02:41:30 +0300

    improve owned mutex

--- OptiScaler/OwnedMutex.h
+++ OptiScaler/OwnedMutex.h
@@ -2,27 +2,33 @@
 
 #include "pch.h"
 
+#include <atomic>
+#include <shared_mutex>
+
 class OwnedMutex {
 private:
-    std::mutex mtx;
-    uint32_t owner{}; // don't use 0
+    std::shared_mutex mtx;
+    std::atomic<uint32_t> owner{}; // don't use 0
 
 public:
     void lock(uint32_t _owner) {
         mtx.lock();
-        owner = _owner;
+        owner.store(_owner, std::memory_order_seq_cst);
     }
 
     // Only unlocks if owner matches
     void unlockThis(uint32_t _owner) {
-        if (owner == _owner) {
-            mtx.unlock();
-            owner = 0;
-        }
+        uint32_t current_owner = owner.load(std::memory_order_seq_cst);
+
+        if (current_owner == 0 || current_owner != _owner)
+            return;
+
+        owner.store(0, std::memory_order_seq_cst);
+        mtx.unlock();
     }
 
     uint32_t getOwner() {
-        return owner;
+        return owner.load(std::memory_order_seq_cst);
     }
 };
 
